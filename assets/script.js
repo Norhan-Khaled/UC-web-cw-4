05464ce41e3423c6a0378648a50549f511a04855
Norhan-Khaled <107856437+Norhan-Khaled@users.noreply.github.com>
2022-06-30 01:10:06 +0300

    loved this CW.
    enjoyed solving it. like a piece of cake !!

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+alert("Welcome to my site! My name is Norhan and i will calculate your grade!");
+
+let grade = prompt("enter your grade");
+alert(grade);
+console.log("%c" + grade, "color: purple");
+
+if (grade >= 90 && grade <= 100) {
+  console.log("لقد حصلت على امتياز 🥳");
+} else if (grade >= 80 && grade <= 89) {
+  console.log("لقد حصلت على جيد جداً🤩");
+} else if (grade >= 70 && grade <= 79) {
+  console.log("لقد حصلت على جيد🙂");
+} else if (grade >= 60 && grade <= 69) {
+  console.log("لقد حصلت على مقبول😕");
+} else if (grade >= 50 && grade <= 59) {
+  console.log("لقد حصلت على ضعيف☹️");
+} else if (grade < 50) {
+  console.log("راسب💔");
+}
+
+// javascript is not the same as java!!
+// there are some commonalities between java and javascript, like how we write a comment for instance
